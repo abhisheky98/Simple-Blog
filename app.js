5d69e7ff20642fd2c82dc8050055386aa6716a75
Abhishek Yadav <12345abhishekyadav@gmail.com>
2022-03-30 20:00:00 +0530

    added delete command

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
+const { render } = require('ejs');
 
 // express app
 const app = express();
@@ -18,6 +19,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'));
 app.use((req, res, next) => {
   res.locals.path = req.path;
@@ -70,9 +72,6 @@ app.get('/about', (req, res) => {
 });
 
 // blog routes
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
 
 app.get('/blogs', (req, res) => {
   Blog.find().sort({ createdAt: -1 })
@@ -83,8 +82,55 @@ app.get('/blogs', (req, res) => {
       console.log(err);
     });
 });
+app.post('/blogs',(req,res) =>{
+  /*console.log(req.body);*/
+  const blog =new Blog(req.body);
+
+  blog.save()
+    .then((result) =>{
+      res.redirect('/blogs');
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+
+})
+app.get('/blogs/:id',(req,res) =>{
+  const id = req.params.id;
+  Blog.findById(id)
+    .then(result => {
+      res.render('details',{blog : result, title :'Blog details'});
+    })
+    .catch(err => {
+      console.log(err)
+    });
+});
+
+app.delete('/blogs/:id',(req,res) =>{
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+  .then(result => {
+    res.json({redirect: '/blogs'})
+
+  })
+  .catch(err=>{
+    console.log(err);
+  })
+})
+
+
+
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
 
 // 404 page
 app.use((req, res) => {
   res.status(404).render('404', { title: '404' });
 });
+
+
+
+
+
